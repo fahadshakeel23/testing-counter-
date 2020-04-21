3e6b3217dcc8b2b505c73eadd4c59cf084bcd4a7
fahadshakeel23 <63012435+fahadshakeel23@users.noreply.github.com>
2020-04-21 19:42:56 +0530

    Add files via upload

---
+++ src/main/java/com/example/counter/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.counter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.support.v4.app.RemoteActionCompatParcelizer;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int mCount = 0;
+    private TextView mShowCount;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mShowCount = (TextView) findViewById(R.id.show_count);
+    }
+
+
+public void showCounter(View view) {
+        Toast toast = Toast.makeText(this, R.string.count, Toast.LENGTH_SHORT);
+              toast.show();
+}
+
+public void countup(View view) {
+        mCount++;
+        if(mShowCount != null)
+            mShowCount.setText(Integer.toString(mCount));
+}
+
+}
